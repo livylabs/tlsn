04f41756c3839888b95236870f416a9da4f1478a
ProtocolWhisper <chrismatai@hotmail.com>
2026-02-26 02:58:03 +0800

    feat: tdx msg specs

--- crates/common/src/msg.rs
+++ crates/common/src/msg.rs
@@ -12,3 +12,31 @@ pub struct ServerIdentityProof {
     /// Server identity data.
     pub data: ServerCertData,
 }
+
+/// TDX attestattion sent from Notary to Prover 
+#[derive(Debug, Serialize , Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TdxAttestation{
+    /// Type of tee attestation
+   pub tdx: TdxEvidence
+}
+#[derive(Debug, Serialize , Deserialize)]
+/// Tee attestation data
+pub struct TdxEvidence{
+    /// Report customs data 64 bytes
+    pub runtime_data: String,
+    /// Tee quote used for verification
+    pub quote: String,
+    /// Anti-replay nonce
+    pub verifier_nonce: VerifierNonce,
+}
+#[derive(Debug, Serialize , Deserialize)]
+/// Extra metadata from the attestation
+pub struct VerifierNonce{
+    /// Random fresh value
+    pub val: String,
+    /// Timestamp
+    pub iat: String,
+    /// Signature from the verifier service
+    pub signature: String,
+}
